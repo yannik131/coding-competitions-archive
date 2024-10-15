12f82b975f460ebf44e5d46fc664201df999478c
yannik131 <yannik131@web.de>
2024-10-15 17:33:29 +0200

    good luck solution, although too slow

--- codejam/2013/round_1a/good_luck/solution.py
+++ codejam/2013/round_1a/good_luck/solution.py
@@ -1,9 +1,10 @@
 import random
-import primefac
 import numpy as np
 from collections import Counter
 from functools import reduce
 from operator import mul
+from math import factorial
+from itertools import combinations_with_replacement, combinations
 
 def multiply_elements(elements):
     return reduce(mul, elements, 1)
@@ -12,17 +13,17 @@ def pad(txt, n=30):
     txt = str(txt)
     return txt + " "*(n-len(txt))
 
-def unique_factorizations(N, M):
-    def factorize(n, upper_limit, current_factors, results):
-        for i in range(2, min(upper_limit, n) + 1):
-            if n % i == 0:
-                new_factors = current_factors + [i]
-                next_n = n // i
-                if next_n == 1:
-                    results.append(sorted(new_factors))
-                else:
-                    factorize(next_n, i, new_factors, results)
+def factorize(n, upper_limit, current_factors, results):
+    for i in range(2, min(upper_limit, n) + 1):
+        if n % i == 0:
+            new_factors = current_factors + [i]
+            next_n = n // i
+            if next_n == 1:
+                results.append(sorted(new_factors))
+            else:
+                factorize(next_n, i, new_factors, results)
 
+def unique_factorizations(N, M):
     results = []
     factorize(N, M, [], results)
     return results
@@ -55,33 +56,52 @@ def generate_products(N, M, K):
             if random.randint(1, 2) == 1:
                 continue
             subset.append(number)
+        #print(f"{pad(subset)} -> {pad(np.prod(subset), 10)}")
         products.append(np.prod(subset))
 
     return original, products
 
-def guess_numbers(N, M, products):
-    total_counter = Counter()
+# Es werden N Zahlen aus einer Menge von K Zahlen ausgewählt: K^N Möglichkeiten
+# Für K = (M - 1) -> (M - 1)^N
+# Betrachten wir eine mögliche Auswahl von 12 Zahlen
+# Es gibt N! Möglichkeiten N Elemente anzuordnen. Sind unter ihnen r, s, ..., t gleiche Elemente
+# gibt es N!/(r!*s!*...*t!) mögliche Anordnungen. Tritt eine Ziffer d N_d mal auf, gibt es also
+# N!/(N_2!*N_3!*...*C_M!) mögliche Anordnungen. Die Wahrscheinlichkeit für eine Auswahl ist daher
+# N!/(N_2!*N_3!*...*C_M!) / (M - 1)^N
+def initial_probability_of(subset, N, M):
+    counter = Counter(subset)
+    demoninator = multiply_elements([(M-1)**N] + [factorial(count) for count in counter.values()])
+    return factorial(N) / demoninator
 
-    for product in products:
-        #print(f"Processing {product}")
-        if product == 1:
-            # this does not tell us anything, ignore
-            continue
-        
-        factorizations = unique_factorizations(product, M)
+def guess_numbers(N, M, K, products):
+    possible_guesses = [tuple(combination) for combination in combinations_with_replacement(range(2, M+1), N)]
+    initial_probabilities = dict() # P(A)
+    product_frequencies = dict() # P(p | A)
+    product_probabilities = dict() # P(p)
+    for guess in possible_guesses:
+        initial_probabilities[guess] = initial_probability_of(guess, N, M)
+        counter = Counter()
+        for i in range(0, N+1):
+            for subset in combinations(guess, i):
+                counter[np.prod(subset)] += 1
+        n_products = sum(counter.values())
+        product_frequencies[guess] = dict()
+        for product, count in counter.items():
+            product_frequencies[guess][product] = count / n_products
+            product_probabilities[product] = 0
 
-        for factorization in factorizations:
-            total_counter += Counter(factorization)
-
-    total_sum = sum(total_counter.values())
-
-    probabilities = dict()
-    for number, count in total_counter.items():
-        probabilities[number] = count / total_sum
+    for product in product_probabilities:
+        for guess in possible_guesses:
+            product_probabilities[product] += initial_probabilities[guess] * product_frequencies[guess].get(product, 0)
     
-    guess = random.choices(list(probabilities.keys()), weights=list(probabilities.values()), k=N)
-    guess = sorted(guess)
+    for product in products:
+        for guess in possible_guesses:
+            conditional_probability = initial_probabilities[guess] * product_frequencies[guess].get(product, 0) \
+                                      / product_probabilities[product] # P(A | p)
 
+            initial_probabilities[guess] *= conditional_probability # P(A) = P(A | p) * P(A) / c_normalization
+
+    guess = list(max(initial_probabilities, key=initial_probabilities.get))
     return guess
 
 def test_guessing(N, M, K, R):
@@ -89,14 +109,17 @@ def test_guessing(N, M, K, R):
 
     for _ in range(R):
         original_numbers, products = generate_products(N, M, K)
-        guess = guess_numbers(N, M, products)
-        #print(f"Guess: {guess} ", end="")
         #print(f"Numbers: {original_numbers}")
+        guess = guess_numbers(N, M, K, products)
+        #print(f"Guess: {guess} ", end="")
         if guess == original_numbers:
             correct += 1
             #print("-> correct!")
 
-    print(f"Guessed correctly {round(correct / R * 100, 1)}% of the time")
-    
-test_subset_generation()
-test_guessing(4, 9, 3, 10000)
+    return correct
+
+assert(test_guessing(3, 5, 7, 100) >= 50)
+print("Ok - 1")
+# this is taking too long with the current
+assert(test_guessing(12, 8, 12, 1) >= 1120)
+print("Ok - 2")
